957fdaab6fada627b81524fe5d271da37f9a80fe
Christian <christian_mahardhika@telkomsel.co.id>
2024-06-21 11:18:36 +0700

    chore: Refactor circuit breaker initialization and client creation

--- circuit-breaker/main.go
+++ circuit-breaker/main.go
@@ -8,27 +8,45 @@ import (
 )
 
 func main() {
-	// Initialize circuit breaker
-	cb := InitCircuitBreaker("consecutive")
+	// Initialize circuit breaker nginx
+	cbNginx := InitCircuitBreaker("consecutive")
+	// Initialize circuit breaker lighttpd
+	cbLighttpd := InitCircuitBreaker("consecutive")
 
-	// Initialize http client with circuit breaker
-	client := InitHttpClient(cb)
+	// Initialize http client nginx with circuit breaker
+	clientNginx := InitHttpClient(cbNginx)
+	// Initialize http client lighttpd with circuit breaker
+	clientLighttpd := InitHttpClient(cbLighttpd)
 
 	for {
-		if !cb.Ready() {
-			fmt.Println("Circuit breaker is open")
+		if !cbNginx.Ready() {
+			fmt.Println("Circuit breaker is open nginx")
 			time.Sleep(2 * time.Second)
-			continue
 		}
 		// Make a request to the server
-		resp, err := client.Get("http://localhost:1111")
+		respNginx, err := clientNginx.Get("http://localhost:1111")
 		if err != nil {
 			// Handle error
 			fmt.Println(err)
 		}
 
 		// Print the response
-		fmt.Println(resp)
+		fmt.Println("nginx", respNginx)
+		time.Sleep(2 * time.Second)
+
+		if !cbLighttpd.Ready() {
+			fmt.Println("Circuit breaker is open Lighttpd")
+			time.Sleep(2 * time.Second)
+		}
+		// Make a request to the server
+		respLighthttpd, err := clientLighttpd.Get("http://localhost:2222")
+		if err != nil {
+			// Handle error
+			fmt.Println(err)
+		}
+
+		// Print the response
+		fmt.Println("Lighttpd", respLighthttpd)
 		time.Sleep(2 * time.Second)
 	}
 }
@@ -38,7 +56,7 @@ func InitCircuitBreaker(breakerType string) (cb *circuit.Breaker) {
 	switch breakerType {
 	case "consecutive":
 		cb = circuit.NewConsecutiveBreaker(
-			int64(10),
+			int64(3),
 		)
 	case "error_rate":
 		cb = circuit.NewRateBreaker(
